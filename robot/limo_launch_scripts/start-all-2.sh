28152c0f02f803a92529faa38831ca0df3d30cd9
Kgratton6 <kevin-santantiago.gratton-fournier@polytml.ca>
2025-03-28 17:56:52 -0400

    changes to launch

--- robot/limo_launch_scripts/start-all-2.sh
+++ robot/limo_launch_scripts/start-all-2.sh
@@ -12,9 +12,9 @@ source install/setup.bash
 colcon build
 source install/setup.bash
 
-ros2 launch limo_bringup limo_start.launch.py id:=limo1 pub_odom_tf:=false & sleep 10
+ros2 launch limo_bringup limo_start.launch.py id:=limo2 pub_odom_tf:=false & sleep 10
 
-ros2 launch limo_bringup cartographer.launch.py & sleep 5
+ros2 launch limo_bringup cartographer.launch.py id:=limo2 & sleep 5
 
-ros2 launch limo_bringup navigation2.launch.py
+ros2 launch limo_bringup navigation2.launch.py id:=limo2
 

6a54f71d148cdbf185586fcf68531b4e095f761f
Kgratton6 <kevin-santantiago.gratton-fournier@polytml.ca>
2025-03-31 10:09:20 -0400

    changes

--- robot/limo/src/limo_ros2/limo_bringup/launch/navigation2.launch.py
+++ robot/limo/src/limo_ros2/limo_bringup/launch/navigation2.launch.py
@@ -22,6 +22,7 @@ from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, ConcatSubstitution, TextSubstitution
 from launch_ros.actions import Node
 
 def generate_launch_description():
